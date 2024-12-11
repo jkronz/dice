6ddd3d82f462f24317f1f8626b415eceb4e3c84a
Justin Kronz <jkronz@gmail.com>
2024-12-11 16:12:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+let total = 0
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(total)
+})
+input.onButtonPressed(Button.B, function () {
+    total = 0
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+    total += number
+})
